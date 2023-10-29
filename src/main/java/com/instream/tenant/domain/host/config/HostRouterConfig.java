0c31beecef6c231645b140318ff8344590e86854
Long9725 <develop9725@gmail.com>
2023-10-30 02:28:16 +0900

    <FEAT> v1 routes

--- src/main/java/com/instream/tenant/domain/host/config/HostRouterConfig.java
+++ src/main/java/com/instream/tenant/domain/host/config/HostRouterConfig.java
@@ -3,17 +3,29 @@ package com.instream.tenant.domain.host.config;
 import com.instream.tenant.domain.host.router.HostRouterHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import java.util.function.Supplier;
+
 import static org.springdoc.webflux.core.fn.SpringdocRouteBuilder.route;
+
 
 @Configuration
 public class HostRouterConfig {
     @Bean
     public RouterFunction<ServerResponse> routes(HostRouterHandler hostRouterHandler) {
-        return route().GET("/hello", hostRouterHandler::hello, ops -> ops
-                .operationId("123")
+        return route().nest(RequestPredicates.path("/v1"),
+                routerFunctionSupplier(hostRouterHandler),
+                ops -> ops.operationId("123")
         ).build();
+    }
+
+    private Supplier<RouterFunction<ServerResponse>> routerFunctionSupplier(HostRouterHandler hostRouterHandler) {
+        return () -> route()
+                .GET("/hello", hostRouterHandler::hello, ops -> ops.operationId("123"))
+                .build();
     }
 }
