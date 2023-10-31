3bfa179dfd9f72be352ae5996cf1286b16c4e977
Long9725 <develop9725@gmail.com>
2023-10-31 11:21:23 +0900

    <FEAT> ReactiveRedisTemplateFactory for type caching

--- src/main/java/com/instream/tenant/domain/host/config/HostRouterConfig.java
+++ src/main/java/com/instream/tenant/domain/host/config/HostRouterConfig.java
@@ -1,6 +1,8 @@
 package com.instream.tenant.domain.host.config;
 
+import com.instream.tenant.domain.host.domain.request.TenantCreateRequest;
 import com.instream.tenant.domain.host.handler.HostHandler;
+import org.springdoc.core.fn.builders.parameter.Builder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RequestPredicates;
@@ -9,6 +11,8 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 
 import java.util.function.Supplier;
 
+import static org.springdoc.core.fn.builders.parameter.Builder.parameterBuilder;
+import static org.springdoc.core.fn.builders.requestbody.Builder.requestBodyBuilder;
 import static org.springdoc.webflux.core.fn.SpringdocRouteBuilder.route;
 
 
@@ -27,7 +31,12 @@ public class HostRouterConfig {
 
     private RouterFunction<ServerResponse> signUpFunctionSupplier(HostHandler hostHandler) {
         return route()
-                .POST("/sign-up", hostHandler::createTenant, ops -> ops.operationId("123"))
+                .POST(
+                        "/sign-up",
+                        hostHandler::createTenant,
+                        ops -> ops.operationId("123")
+                                .requestBody(requestBodyBuilder().implementation(TenantCreateRequest.class))
+                )
                 .build();
     }
 
