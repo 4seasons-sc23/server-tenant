904e9f562faea9b78490f41c75419fd8a2e088c8
Long9725 <develop9725@gmail.com>
2023-11-03 15:57:34 +0900

    <FEAT> disable write timestamp to integer array

--- src/main/java/com/instream/tenant/core/config/WebConfig.java
+++ src/main/java/com/instream/tenant/core/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.instream.tenant.core.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +15,10 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 public class WebConfig implements WebFluxConfigurer {
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper().registerModule(new JavaTimeModule());
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
     }
 
     @Override
