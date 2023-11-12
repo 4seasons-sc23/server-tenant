74e668e0d3dd63e65f8e19887dec0fe80c5e28d3
Long9725 <develop9725@gmail.com>
2023-11-13 00:21:15 +0900

    <REFACTOR> change table annotation name from upper snake case to lower snake case

--- src/main/java/com/instream/tenant/domain/application/domain/entity/ApplicationEntity.java
+++ src/main/java/com/instream/tenant/domain/application/domain/entity/ApplicationEntity.java
@@ -11,7 +11,7 @@ import org.springframework.data.relational.core.mapping.Table;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Table(name = "APPLICATIONS")
+@Table(name = "applications")
 @Setter
 @Getter
 @Builder
