38593ac41d4f66e09045767fcba57805e58436e9
Yoo Jin <dbwls30131@ajou.ac.kr>
2023-11-17 00:45:22 +0900

    <FEAT> ServiceError ErrorCode Enum 생성

--- src/main/java/com/instream/tenant/domain/serviceError/infra/enums/ServiceErrorErrorCode.java
+++ src/main/java/com/instream/tenant/domain/serviceError/infra/enums/ServiceErrorErrorCode.java
@@ -1,2 +1,29 @@
-package com.instream.tenant.domain.serviceError.infra.enums;public enum ServiceErrorErrorCode {
+package com.instream.tenant.domain.serviceError.infra.enums;
+
+import com.instream.tenant.domain.error.infra.enums.HttpErrorCode;
+import org.springframework.http.HttpStatus;
+
+public enum ServiceErrorErrorCode implements HttpErrorCode {
+    SERVICE_ERROR_NOT_FOUND("문의내역을 찾지 못 했습니다.", HttpStatus.NOT_FOUND);
+
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    ServiceErrorErrorCode(String message, HttpStatus httpStatus) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getCode() {
+        return name();
+    }
 }
