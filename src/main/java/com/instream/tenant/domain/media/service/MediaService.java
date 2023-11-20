a1a9ec8eb59f3d4577f828f5f220b7522af439a9
Long9725 <develop9725@gmail.com>
2023-11-20 19:13:18 +0900

    <REFACTOR> parsing main m3u8 for converting _ to /

--- src/main/java/com/instream/tenant/domain/media/service/MediaService.java
+++ src/main/java/com/instream/tenant/domain/media/service/MediaService.java
@@ -7,9 +7,21 @@ import com.instream.tenant.domain.application.repository.ApplicationSessionRepos
 import com.instream.tenant.domain.media.domain.request.MediaUploadRequestDto;
 import com.instream.tenant.domain.minio.MinioService;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.WritableByteChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -54,16 +66,92 @@ public class MediaService {
                         return Mono.just("ts is null");
                     }
 
-                    Mono<String> uploadMainM3u8 = minioService.uploadFile(savedPath + "/index.m3u8", uploadRequest.m3u8Main(),
-                            "application/vnd.apple.mpegurl");
-                    Mono<String> uploadM3u8 = minioService.uploadFile(savedPath + "/" + uploadRequest.quality() + "/" + "index.m3u8",
-                            uploadRequest.m3u8(), "application/vnd.apple.mpegurl");
-                    Mono<String> uploadTs = minioService.uploadFile(savedPath + "/" + uploadRequest.quality() + "/" + uploadRequest.ts().filename(),
-                            uploadRequest.ts(), "video/MP2T");
+                    Mono<String> uploadMainM3u8 = convertM3u8ToFile(uploadRequest.m3u8Main(), savedPath)
+                            .flatMap(file -> minioService.uploadFile(savedPath + "/index.m3u8", file,
+                                    "application/vnd.apple.mpegurl"));
+                    Mono<String> uploadM3u8 = convertToFile(uploadRequest.m3u8(), savedPath)
+                            .flatMap(file -> minioService.uploadFile(savedPath + "/" + uploadRequest.quality() + "/" + "index.m3u8",
+                                    file, "application/vnd.apple.mpegurl"));
+                    Mono<String> uploadTs = convertToFile(uploadRequest.ts(), savedPath)
+                            .flatMap(file -> minioService.uploadFile(savedPath + "/" + uploadRequest.quality() + "/" + uploadRequest.ts().filename(),
+                                    file, "video/MP2T"));
 
                     return Mono.zip(uploadMainM3u8, uploadM3u8, uploadTs);
                 })
                 .thenReturn("File uploaded successfully");
     }
 
+    private Mono<File> convertToFile(FilePart filePart, String sessionId) {
+        return Mono.create(sink -> {
+            try {
+                // 임시 파일 생성
+                File tempFile = File.createTempFile(String.format("temp_%s", sessionId), filePart.filename());
+                WritableByteChannel channel = Channels.newChannel(new FileOutputStream(tempFile));
+
+                // FilePart의 내용을 임시 파일에 쓴다
+                filePart.content().subscribe(dataBuffer -> {
+                    try {
+                        byte[] bytes = new byte[dataBuffer.readableByteCount()];
+                        dataBuffer.read(bytes);
+                        channel.write(ByteBuffer.wrap(bytes));
+                    } catch (IOException e) {
+                        sink.error(e);
+                    } finally {
+                        DataBufferUtils.release(dataBuffer);
+                    }
+                }, sink::error, () -> {
+                    try {
+                        channel.close();
+                        sink.success(tempFile);
+                    } catch (IOException e) {
+                        sink.error(e);
+                    }
+                });
+            } catch (IOException e) {
+                sink.error(e);
+            }
+        });
+    }
+
+    private Mono<File> convertM3u8ToFile(FilePart filePart, String sessionId) {
+        return Mono.create(sink -> {
+            try {
+                // 임시 파일 생성
+                File tempFile = File.createTempFile(String.format("temp_main_%s", sessionId), ".m3u8");
+
+                // FilePart의 내용을 임시 파일에 쓴다
+                filePart.content()
+                        .map(dataBuffer -> {
+                            byte[] bytes = new byte[dataBuffer.readableByteCount()];
+                            dataBuffer.read(bytes);
+                            DataBufferUtils.release(dataBuffer);
+                            return ByteBuffer.wrap(bytes);
+                        })
+                        .collectList()
+                        .flatMap(list -> Mono.fromCallable(() -> {
+                            try (WritableByteChannel channel = Channels.newChannel(new FileOutputStream(tempFile))) {
+                                for (ByteBuffer byteBuffer : list) {
+                                    channel.write(byteBuffer);
+                                }
+                            }
+                            return tempFile;
+                        }))
+                        .flatMap(file -> replaceTextInFile(file, String.format("%s_", sessionId), String.format("%s/", sessionId)))
+                        .subscribe(sink::success, sink::error);
+            } catch (IOException e) {
+                sink.error(e);
+            }
+        });
+    }
+
+    private Mono<File> replaceTextInFile(File file, String searchText, String replaceText) {
+        return Mono.fromCallable(() -> {
+            Path path = file.toPath();
+            String content = Files.lines(path)
+                    .map(line -> line.replace(searchText, replaceText))
+                    .collect(Collectors.joining("\n"));
+            Files.write(path, content.getBytes(), StandardOpenOption.WRITE);
+            return file;
+        });
+    }
 }
