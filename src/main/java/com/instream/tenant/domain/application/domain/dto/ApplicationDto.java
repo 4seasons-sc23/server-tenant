8e009cda387add753a7761405a97adaeae8b6459
Long9725 <develop9725@gmail.com>
2023-11-30 22:14:20 +0900

    <FIX> billing dto mapping query

--- src/main/java/com/instream/tenant/domain/application/domain/dto/ApplicationDto.java
+++ src/main/java/com/instream/tenant/domain/application/domain/dto/ApplicationDto.java
@@ -5,30 +5,39 @@ import com.instream.tenant.domain.common.infra.enums.Status;
 import com.querydsl.core.annotations.QueryProjection;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record ApplicationDto(
-        @Schema(description = "어플리케이션 ID", example = "80bd6328-76a7-11ee-b720-0242ac130003")
-        UUID id,
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+public class ApplicationDto {
+    @Schema(description = "어플리케이션 ID", example = "80bd6328-76a7-11ee-b720-0242ac130003")
+    private UUID id;
 
-        @Schema(description = "어플리케이션 종류", examples = {"CHAT", "STREAMING"})
-        ApplicationType type,
+    @Schema(description = "어플리케이션 종류", examples = {"CHAT", "STREAMING"})
+    private ApplicationType type;
 
-        @Schema(description = "어플리케이션 상태(N - 활성화, Y - 삭제, P - 비활성화, F - 정지)", examples = {"N", "Y", "P", "F"})
-        Status status,
+    @Schema(description = "어플리케이션 상태(N - 활성화, Y - 삭제, P - 비활성화, F - 정지)", examples = {"N", "Y", "P", "F"})
+    private Status status;
 
-        @Schema(description = "어플리케이션 생성 시간", example = "2023-11-26T01:59:44.885Z")
-        LocalDateTime createdAt,
+    @Schema(description = "어플리케이션 생성 시간", example = "2023-11-26T01:59:44.885Z")
+    private LocalDateTime createdAt;
 
-        @Schema(description = "어플리케이션 세션")
-        ApplicationSessionDto session
+    @Schema(description = "어플리케이션 세션")
+    private ApplicationSessionDto session;
 
-) {
-    @Builder
     @QueryProjection
-    public ApplicationDto {
-
+    public ApplicationDto(UUID id, ApplicationType type, Status status, LocalDateTime createdAt, ApplicationSessionDto session) {
+        this.id = id;
+        this.type = type;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.session = session;
     }
 }
--- src/main/java/com/instream/tenant/domain/billing/domain/dto/BillingDto.java
+++ src/main/java/com/instream/tenant/domain/billing/domain/dto/BillingDto.java
@@ -4,34 +4,42 @@ import com.instream.tenant.domain.application.domain.dto.ApplicationDto;
 import com.instream.tenant.domain.common.infra.enums.Status;
 import com.querydsl.core.annotations.QueryProjection;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record BillingDto(
-        @Schema(description = "사용량 ID", example = "80bd6328-76a7-11ee-b720-0242ac130003")
-        UUID id,
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+public class BillingDto {
+    @Schema(description = "사용량 ID", example = "80bd6328-76a7-11ee-b720-0242ac130003")
+   private UUID id;
 
-        @Schema(description = "사용량 요금", example = "123123.123123")
-        double cost,
+    @Schema(description = "사용량 요금", example = "123123.123123")
+    private double cost;
 
-        @Schema(description = "사용량 상태(N - 결제 완료, Y - 삭제, P - 결제 대기중, F - 사용량 추적 중(사용자한테 노출 X))", examples = {"N", "Y", "P", "F"})
-        Status status,
+    @Schema(description = "사용량 상태(N - 결제 완료, Y - 삭제, P - 결제 대기중, F - 사용량 추적 중(사용자한테 노출 X))", examples = {"N", "Y", "P", "F"})
+    private Status status;
 
 
-        @Schema(description = "사용량 생성 시간", example = "2023-11-26T02:27:20.492Z")
-        LocalDateTime createdAt,
+    @Schema(description = "사용량 생성 시간", example = "2023-11-26T02:27:20.492Z")
+    private LocalDateTime createdAt;
 
-        @Schema(description = "사용량 수정 시간", example = "2023-11-26T02:27:20.492Z")
-        LocalDateTime updatedAt,
+    @Schema(description = "사용량 수정 시간", example = "2023-11-26T02:27:20.492Z")
+    private LocalDateTime updatedAt;
 
-        @Schema(description = "어플리케이션")
-        ApplicationDto application
-) {
-    @Builder
+    @Schema(description = "어플리케이션")
+    private ApplicationDto application;
+
     @QueryProjection
-    public BillingDto {
-
+    public BillingDto(UUID id, double cost, Status status, LocalDateTime createdAt, LocalDateTime updatedAt, ApplicationDto application) {
+        this.id = id;
+        this.cost = cost;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.application = application;
     }
 }
